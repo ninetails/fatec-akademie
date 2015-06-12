4c3269dc341d494dee4ae6a08132f071705007d9
Carlos Kazuo <ninetails.nine@gmail.com>
2015-06-12 04:56:52 +0000

    cleaning

--- www/js/akademie/controllers.js
+++
@@ -1,7 +0,0 @@
-angular.module('akademie.controllers', [])
-
-.controller('UserController', function($scope, $location, userLogged) {
-  if (!userLogged.is_logged()) {
-    $location.path('/login');
-  }
-});
