9fd444c62ff122b195b35246b63b5c926697e474
Carlos Kazuo Mochizuki Ishizaka <carlos.kazuo@grupofolha.com.br>
2015-06-18 18:47:26 -0300

    added cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 var util = require('util');
 var express = require('express');
+var cors = require('cors');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 
+// middlewares
+app.use(cors());
 app.use(morgan());
-app.use(express.static('www'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// static
+app.use(express.static('www'));
+
+// custom middleware
 app.use((req, res, next) => {
   if ( !!req.headers && !!req.headers['x-request-source'] ) {
     req.origin = req.headers['x-request-source'] === 'app' ? 'app' : 'web' ;
@@ -15,12 +21,16 @@ app.use((req, res, next) => {
   next();
 });
 
+// modules
 app.use('/', require('./modules/users'));
 
+
+// index
 app.get('/', function (req, res) {
   res.sendFile('www/index.html');
 });
 
+// server
 var server = app.listen(3000, function () {
   console.log(util.format('app listening at port %s', server.address().port));
 });
